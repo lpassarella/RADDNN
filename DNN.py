f96e824067a1bd54c57a1e4c25de18ddb2356542
Linsey Passarella <lpass@cori04.nersc.gov>
2019-10-03 12:04:25 -0700

    update

--- DNN.py
+++ DNN.py
@@ -11,18 +11,26 @@ config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.5 # maximun alloc gpu50% of MEM
 config.gpu_options.allow_growth = True #allocate dynamically
 
+import numpy as np
+rad_out=np.load('rad_out.npy')
+rad_in=np.load('rad_in.npy')
+print(np.shape(rad_out))
+rad_out=rad_out[:,:,1:]
+rad_in=rad_in[:,:,1:]
+rad_out=np.reshape(rad_out,(rad_out.shape[0]*rad_out.shape[1],rad_out.shape[2]))
+import pandas as pd
+rad_in=np.reshape(rad_in,(rad_in.shape[0]*rad_in.shape[1],rad_in.shape[2]))
+print(np.shape(rad_out))
+rad_out=pd.DataFrame(rad_out)
+rad_in=pd.DataFrame(rad_out)
 
-train_X= np.asarray(np.load('train_X.npy'))
-train_Y= np.asarray(np.load('train_Y.npy'))
+from sklearn.model_selection import train_test_split
+train_X, test_X, train_Y, test_Y = train_test_split(rad_in,rad_out, test_size=0.2, random_state=42)
 
-test_X=np.asarray(np.load('test_X.npy'))
-test_Y=np.asarray(np.load('test_Y.npy'))
-print(train_X.shape, test_X.shape)
-print(train_Y.shape, test_Y.shape)
-train_X=np.reshape(train_X,(train_X.shape[0]*train_X.shape[1],train_X.shape[2]))
-train_Y=np.reshape(train_Y,(train_Y.shape[0]*train_Y.shape[1],train_Y.shape[2]))
-test_X=np.reshape(test_X,(test_X.shape[0]*test_X.shape[1],test_X.shape[2]))
-test_Y=np.reshape(test_Y,(test_Y.shape[0]*test_Y.shape[1],test_Y.shape[2]))
+train_X=train_X.values
+train_Y = train_Y.values
+test_X=test_X.values
+test_Y=test_Y.values
 
 XMEAN=np.max(train_X,axis=0)
 XSTDD=np.min(train_X,axis=0)
@@ -75,13 +83,13 @@ classifier.add(Dense(out_n_cols,kernel_initializer='uniform',
 classifier.compile(optimizer='adam',loss='mean_squared_error',metrics=['accuracy'])
 
 
-classifier.fit(train_X,train_Y, validation_split=0.1,batch_size=256,epochs=400)
+classifier.fit(train_X,train_Y, validation_split=0.1,batch_size=1024,epochs=100)
 
 score  = classifier.predict(test_X)
 print(score.shape)
 for j in range(out_n_cols):
  score[:,j]=score[:,j]*(YMEAN[j]-YSTDD[j])+YSTDD[j]
-
+classifier.save('my_model.h5')
 #summary = model.summary()
 W_Input_Hidden0 = classifier.layers[0].get_weights()[0];
 biases0  = classifier.layers[0].get_weights()[1];
@@ -89,8 +97,7 @@ W_Input_Hidden1 = classifier.layers[1].get_weights()[0];
 biases1  = classifier.layers[1].get_weights()[1];
 W_Input_Hidden2 = classifier.layers[2].get_weights()[0];
 biases2  = classifier.layers[2].get_weights()[1];
-W_Input_Hidden3 = classifier.layers[3].get_weights()[0];
-biases3  = classifier.layers[3].get_weights()[1];
+
 
 np.save('SWHidden001.npy',W_Input_Hidden0)
 np.save('SWbiases001.npy',biases0)
@@ -112,3 +119,5 @@ np.save('train_loss.npy',train_loss)
 np.save('val_loss.npy',val_loss)
 np.save('train_acc.npy',acc_train)
 np.save('val_acc.npy',acc_val)
+
+
